e20904b78bf1a3896b3052f47d54d28dd5f96541
fewpudo <filipemaluhy@gmail.com>
2024-04-23 14:55:53 -0300

    build label matrix

--- main.py
+++ main.py
@@ -14,15 +14,9 @@ from utils import selectors, workers
 # Plotar imagens filtrados com CAR e filtrado sem CAR.
 # Verificar se o filtro notch é implementado pela placa e também se é disponibilizado pela API.
 
-# Separar cada vetor para cada canal, O1(8), O1(10), O1(12), O1(15) , OZ(8), OZ(10)... -> Done
-# y = ax + 1x = (a+1)x
-# matrix A tem 13 colunas ao inves de 12, sendo a última coluna uma coluna de 1s (13 x 120) -> Done
-# separar minha matriz de atributos e colocar uma coluna de 1s no final -> Done
 
 # func trainTestSplit(X, y, test_size=0.2, random_state=None) -> É bom já ter construído o Y antes de chamar essa função.
-# Gerar um handle para pegar a quantidade de amostra, não posso treinar com tudo pois tenho que validar. Escolher de forma aleatória. -> Done
 
-# Basicamente separar minha matriz X em X_treino e X_validação, sendo 96x13 e 24x13 respectivamente. -> Done
 # W tem 13x1 e Y tem a mesmo número de linhas de X
 
 data = scipy.io.loadmat('subjects/S1.mat')
--- utils/workers.py
+++ utils/workers.py
@@ -6,3 +6,43 @@ def buildValidationAndTestMatrix(data):
     validationMatrix = data[np.random.choice(data.shape[0], 24, replace=False), :]
     testMatrix = data[np.random.choice(np.setdiff1d(np.arange(data.shape[0]), validationMatrix[:, 0]), 96, replace=False), :]
     return validationMatrix, testMatrix
+
+
+def buildY8Matrix():
+    y_matrix = np.ones((120, 1))
+    y_matrix[:30] = 1
+    y_matrix[30:60] = -1
+    y_matrix[60:90] = -1
+    y_matrix[90:] = -1
+    return y_matrix
+
+def buildY10Matrix():
+    y_matrix = np.ones((120, 1))
+    y_matrix[:30] = -1
+    y_matrix[30:60] = 1
+    y_matrix[60:90] = -1
+    y_matrix[90:] = -1
+    return y_matrix
+
+def buildY12Matrix():
+    y_matrix = np.ones((120, 1))
+    y_matrix[:30] = -1
+    y_matrix[30:60] = -1
+    y_matrix[60:90] = 1
+    y_matrix[90:] = -1
+    return y_matrix
+
+def buildY15Matrix():
+    y_matrix = np.ones((120, 1))
+    y_matrix[:30] = -1
+    y_matrix[30:60] = -1
+    y_matrix[60:90] = -1
+    y_matrix[90:] = 1
+    return y_matrix
+
+def buildYMatrix():
+    y8 = buildY8Matrix()
+    y10 = buildY10Matrix()
+    y12 = buildY12Matrix()
+    y15 = buildY15Matrix()
+    return y8, y10, y12, y15
