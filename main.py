d8210ad49f33ece1fb136fc25c8aca9c93374bdf
fewpudo <filipemaluhy@gmail.com>
2024-09-12 01:43:22 -0300

    ajust in the main

--- main.py
+++ main.py
@@ -3,30 +3,28 @@ from brainflow.data_filter import DataFilter, FilterTypes, AggOperations
 import time
 import numpy as np
 import matplotlib.pyplot as plt
-from data import DataMain
+from data import plotAllChannelsFFTData, buildFFTData
+from openbci import cyton as bci
 
 # Configurações da Cyton
-params = BrainFlowInputParams()
-params.serial_port = 'COM8'  # Substitua pelo seu serial port (em Windows pode ser COM3, por exemplo)
 
-board = BoardShim(BoardIds.CYTON_BOARD.value, params)
+board = bci.OpenBCICyton(daisy=False)
+
 
 # Inicialize a sessão
-#board.release_session()
 try:
     board.prepare_session()
 
     board.start_stream()
-    print("Aquisitando dados...")
+    for i in range(7):
+        data = board.get_board_data(250)
+        time.sleep(1.5)
+        data_eeg = data[1:9,:]
+        plotAllChannelsFFTData(buildFFTData(data_eeg))
 
-    data = board.get_board_data(1250)
-    time.sleep(5)
+
     board.stop_stream()
-
     board.release_session()
-
-    data_eeg = data[1:9,:]
-    DataMain(data_eeg)
-
-except:
+except Exception as e:
+    print("Ocorreu um erro durante a aquisição de dados:", str(e))
     board.release_session()
